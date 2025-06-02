3605226387527e04e609b5590830b89d21b572ea
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2025-06-02 11:37:59 +0200

    Exercices Rappels

--- hello-storybook/src/stories/Select.jsx
+++ hello-storybook/src/stories/Select.jsx
@@ -1,6 +1,14 @@
 import { useState } from 'react';
 import './select.css';
 
+/** Select Component
+ * A simple select component that allows users to choose an item from a dropdown menu.
+ * 
+ * Props:
+ * - value: The currently selected value.
+ * - items: An array of items to display in the dropdown.
+ * - onSelected: Callback function to handle item selection.
+ */
 function Select(props) {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
